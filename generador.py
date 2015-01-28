93072c2070b865dcb417b64a7fd39b37926a4af6
lzerokoo <jsjuanes51@gmail.com>
2015-01-27 21:07:45 -0500

    Actualización y comentarios

--- generador.py
+++ generador.py
@@ -1,17 +1,19 @@
-def generador(li=[1,2,3,4,5,6],indice=5,ranA=1,ranB=45):
-    #5 864 443 200
-    #8 145 060
-    result=[]
-    #gen=range(ranA,ranB+1)
-    count=0
+# -*- coding: utf-8 -*-
+def generador(li=[1,2,3,4,5,6],indice=5):
+    #5 864 443 200 nPr
+    #8 145 060     nCr
+    result=[] #lista para almazenar resultados
+    count=0 #inicialización de variables
     terminar=0
     while True:
         if li[indice]==45:
             li[indice]=1
             li[indice-1]+=1
+            #reloj basico, cuando la ultima posición llego a limite, 45; esta vuelve a valer 1, y suma uno a la anterior poscisión
         else:
             li[indice]+=1
         if li[indice-1] == 45:
+            #condicional para no avanzar a mas de 45,y saber donde parar
             if li[indice-5]+li[indice-4]+li[indice-3]+li[indice-2]+li[indice-1]==45*5:
                 pass
             else:
@@ -38,16 +40,18 @@ def generador(li=[1,2,3,4,5,6],indice=5,ranA=1,ranB=45):
         if li[indice-5]==45*1:
             pass
         if li==[45,44,43,42,41,40]:
+            #donde terminar la creacion de valores
             break
+        
         #Eliminacion de numeros repetidos
-
+        #se dejan solo las combinaciones
         if li[5]>li[4]>li[3]>li[2]>li[1]>li[0]:
             result.append(li)
-                ## tester
+            ## teste0
             terminar+=1
             #debug
             print(terminar,li)
-        #debug
+            #debug
         if terminar==1000:
             break
         #end tester
